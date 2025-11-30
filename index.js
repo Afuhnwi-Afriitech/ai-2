e2bda19eb78ea6783e4fdb8270073909b6a6334f
aPëx prēdaTør <69242741+Afuhnwi-Afriitech@users.noreply.github.com>
2025-11-30 11:07:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const OpenAI = require("openai");
 const express = require('express');
 const app = express();
 const path = require('path');
-const {
-  marked
-} = require('marked');
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
